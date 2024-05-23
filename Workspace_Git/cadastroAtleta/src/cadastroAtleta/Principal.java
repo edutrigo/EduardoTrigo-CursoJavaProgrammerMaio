f93dc8bc6d536d68dd8997ce5423ffa12339afce
edutrigo <edutrigo@gmail.com>
2024-05-23 11:48:33 -0300

    inclusao do projeto Animal e Atleta

---
+++ Workspace_Git/cadastroAtleta/src/cadastroAtleta/Principal.java
@@ -0,0 +1,42 @@
+package cadastroAtleta;
+
+import java.util.Scanner; //Objeto java padrao para receber dados atraves do teclado
+import entidade.Atleta;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Atleta objAtleta = new Atleta();
+		
+		Scanner objEntradaDados = new Scanner(System.in);
+		
+		System.out.println( "Informe o nome do Atleta:");
+		objAtleta.v_nome = objEntradaDados.next();
+		
+		System.out.println( "Informe a idade do Atleta:");
+		objAtleta.v_idade = Integer.parseInt(objEntradaDados.next());
+
+		System.out.println( "Informe a qtd de partidas:");
+		objAtleta.v_qtd_partidas = Integer.parseInt(objEntradaDados.next());
+
+		objAtleta.v_salario = calcularSalario(objAtleta.v_qtd_partidas);
+		
+		imprimir( objAtleta );
+		
+		imprimirResultadosNoConsole(objetoAtleta); // Chamo a execução do metodo para imprimir os resultados
+	}
+
+	public static void imprimirResultadosNoConsole(Atleta objAtleta) {
+		//Resultado Final
+		System.out.println("Dados..."); //Display na tela
+		System.out.println("Nome:" + objAtleta.v_nome + " - Idade:" + objAtleta.v_idade + " - Perna:" + objAtleta.v_salario );
+		}
+
+	public static Double calcularSalario(int qtdPartida) {
+		
+		double salario = qtdPartida * 10000;
+		
+		return salario;
+	}
+
+}
---
+++ Workspace_Git/cadastroAtleta/src/entidade/Atleta.java
@@ -0,0 +1,8 @@
+package entidade;
+
+public class Atleta {
+	public String v_nome;         //Inicializa variavel
+	public int    v_idade;        //Inicializa variavel
+	public double v_salario;      //Inicializa variavel
+	public int    v_qtd_partidas; //Inicializa variavel
+}
