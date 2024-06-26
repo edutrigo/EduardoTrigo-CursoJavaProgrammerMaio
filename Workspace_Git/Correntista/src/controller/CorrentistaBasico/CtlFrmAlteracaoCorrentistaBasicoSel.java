b697395402c082dba9dd42dc5b83d230567915fe
edutrigo <edutrigo@gmail.com>
2024-06-25 23:09:39 -0300

    ALTERAÇÃO CORRENTISTA BASICO - SÓ FALTAR SALVAR AS MUDANÇAS!!

--- Workspace_Git/Correntista/src/controller/CorrentistaBasico/CtlFrmAlteracaoCorrentistaBasicoSel.java
+++ Workspace_Git/Correntista/src/controller/CorrentistaBasico/CtlFrmAlteracaoCorrentistaBasicoSel.java
@@ -4,7 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import entity.EntCorrentistaBasico;
+import form.CorrentistaBasico.FrmAlteracaoCorrentistaBasicoManut;
+import repository.RepCorrentistaBasicoImplement;
 
 public class CtlFrmAlteracaoCorrentistaBasicoSel implements ActionListener {
 
@@ -19,9 +24,18 @@ public class CtlFrmAlteracaoCorrentistaBasicoSel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand() == "ALTERAR") {
-			// RepCorrentistaBasicoImplement repCorrentistaBasicoImplement = new
-			// RepCorrentistaBasicoImplement();
-			// repCorrentistaBasicoImplement.excluiCorrentista(cpf.getText());
+
+			RepCorrentistaBasicoImplement repCorrentistaBasicoImplement = new RepCorrentistaBasicoImplement();
+
+			if (repCorrentistaBasicoImplement.buscaCorrentistaPorCpf(cpf.getText()) != null) {
+				EntCorrentistaBasico entCorrentistaBasico = new EntCorrentistaBasico();
+				entCorrentistaBasico = repCorrentistaBasicoImplement.buscaCorrentistaPorCpf(cpf.getText());
+
+				FrmAlteracaoCorrentistaBasicoManut frmAlteracaoCorrentistaBasicoManut = new FrmAlteracaoCorrentistaBasicoManut();
+				frmAlteracaoCorrentistaBasicoManut.alteracao(entCorrentistaBasico);
+			} else {
+				JOptionPane.showMessageDialog(null, "CPF não encontrado...");
+			}
 
 		} else if (e.getActionCommand() == "VOLTAR") {
 			frameRetornar.setVisible(false);
---
+++ Workspace_Git/Correntista/src/form/CorrentistaBasico/FrmAlteracaoCorrentistaBasicoManut.java
@@ -0,0 +1,86 @@
+package form.CorrentistaBasico;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import controller.CorrentistaBasico.CtlFrmInclusaoCorrentistaBasico;
+import entity.EntCorrentistaBasico;
+import entity.EntEndereco;
+
+public class FrmAlteracaoCorrentistaBasicoManut {
+
+	public void alteracao(EntCorrentistaBasico entCorrentista) {
+		//
+		GridLayout grid = new GridLayout(0, 1);
+		JFrame frmCorrentista = new JFrame();
+		frmCorrentista.setSize(200, 350);
+		JPanel pnCorrentista = new JPanel();
+		pnCorrentista.setLayout(grid);
+		frmCorrentista.add(pnCorrentista);
+
+		// NOME
+		JLabel lbNome = new JLabel("Nome");
+		pnCorrentista.add(lbNome);
+		JTextField tfNome = new JTextField(10);
+		pnCorrentista.add(tfNome);
+		tfNome.setText(entCorrentista.getNome());
+
+		// CPF
+		JLabel lbCpf = new JLabel("CPF");
+		pnCorrentista.add(lbCpf);
+		JTextField tfCpf = new JTextField(10);
+		pnCorrentista.add(tfCpf);
+		tfCpf.setText(entCorrentista.getCpf());
+
+		// CEP
+		EntEndereco entEndereco = new EntEndereco();
+		entEndereco = entCorrentista.getEndereco();
+
+		JLabel lbCep = new JLabel("CEP");
+		pnCorrentista.add(lbCep);
+		JTextField tfCep = new JTextField(10);
+		pnCorrentista.add(tfCep);
+		tfCep.setText(entEndereco.getCep());
+
+		// EMAIL
+		JLabel lbEmail = new JLabel("EMAIL");
+		pnCorrentista.add(lbEmail);
+		JTextField tfEmail = new JTextField(10);
+		pnCorrentista.add(tfEmail);
+		tfEmail.setText(entCorrentista.getEmail());
+
+		// QTD TRANSAÇÃO
+		JLabel lbQtdTrans = new JLabel("QTD TRANSAÇÃO");
+		pnCorrentista.add(lbQtdTrans);
+		JTextField tfQtdTrans = new JTextField(10);
+		pnCorrentista.add(tfQtdTrans);
+		tfQtdTrans.setText(Integer.toString(entCorrentista.getQtdTransacao()));
+
+		// LIMITE SAQUE
+		JLabel lbLimSaque = new JLabel("LIM SAQUE");
+		pnCorrentista.add(lbLimSaque);
+		JTextField tfLimSaque = new JTextField(10);
+		pnCorrentista.add(tfLimSaque);
+		tfLimSaque.setText(Double.toString(entCorrentista.getLimiteDeSaque()));
+
+		//
+		JButton svBottom = new JButton("Salvar");
+		pnCorrentista.add(svBottom);
+
+		JButton bkBottom = new JButton("Voltar");
+		pnCorrentista.add(bkBottom);
+
+		CtlFrmInclusaoCorrentistaBasico ctlFrmInclusao = new CtlFrmInclusaoCorrentistaBasico(tfNome, tfCpf, tfCep,
+				tfEmail, tfQtdTrans, tfLimSaque, frmCorrentista);
+
+		svBottom.addActionListener(ctlFrmInclusao);
+		bkBottom.addActionListener(ctlFrmInclusao);
+
+		frmCorrentista.setVisible(true);
+	}
+}
